2f9510508c3643b2763b6d6588a8375a5f09d622
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-11-26 12:42:02 +0530

    redux: another way to intentionally render the componet on redux store update by doing the dom manupulation

--- 01-todo-app/src/components/Counter1.jsx
+++ 01-todo-app/src/components/Counter1.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import store from "../counterStore";
 // const Counter1 = ({ count, setCount }) => {
 const Counter1 = () => {
   const count = store.getState().count;
-  const [countState, setCountState] = useState(0);
+  // const [countState, setCountState] = useState(0);
 
   console.log("count from Counter1 Component :", count);
   const incrementCount = () => {
     console.log("incrementCount");
     store.dispatch();
-    setCountState(store.getState().count);
+    // setCountState(store.getState().count);
+    let newCount = store.getState().count;
+    document.getElementById("count1").innerText = `Count : ${newCount}`;
   };
   return (
     <div>
-      <h2>Count : {count}</h2>
+      <h2 id="count1">Count : {count}</h2>
       <div>
         <button onClick={incrementCount}>Increment</button>
         <button>Decrement</button>
